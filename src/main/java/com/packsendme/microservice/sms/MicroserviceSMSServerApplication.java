e9b7e112e6e59898fe549491576bef56fb3ce8c4
RLibera <RLibera@LCENTRAL1924.GrupoAdeslas.2k3>
2021-08-12 16:26:13 +0200

    Change Version 000V

--- src/main/java/com/packsendme/microservice/sms/MicroserviceSMSServerApplication.java
+++ src/main/java/com/packsendme/microservice/sms/MicroserviceSMSServerApplication.java
@@ -2,17 +2,23 @@ package com.packsendme.microservice.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-//@EnableCaching
-//@EnableScheduling
+@EnableCaching
+@EnableScheduling
 public class MicroserviceSMSServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("------------------------------------------------");
+		System.out.println(" MicroserviceSMSServerApplication  ");
+		System.out.println("------------------------------------------------");
+
 		SpringApplication.run(MicroserviceSMSServerApplication.class, args);
 	}
 	
